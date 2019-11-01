668c868b9ffbef08feb4cdfab8dc23f8ec8ae262
Nicolas Machare <pmachare@baufest.com>
2019-11-01 14:23:54 -0300

    practica jquery

--- clase 6/Scripts/_references.js
+++ clase 6/Scripts/_references.js
@@ -1,2 +1,3 @@
 ﻿/// <reference path="qunit.js" />
 /// <reference path="jquery-3.4.1.js" />
+/// <reference path="c:/Users/NMachare/Documents/html/clase 6/CalculatorLibrary.js" />
--- clase 6/test.js
+++ clase 6/test.js
@@ -1,43 +1,20 @@
-test("Object Literal Test", function () {
-    expect(2);
-    var expected = 'Vehicle: 2000 Ford Fusion';
-    var actual = car1.getInfo();
-    equal(actual, expected, 'Expected value: ' + expected +
-    ' Actual value: ' + actual);
-    var expected = 'Vehicle: 2010 BMW Z4';
-    var actual = car2.getInfo();
-    equal(actual, expected, 'Expected value: ' + expected +
-    ' Actual value: ' + actual);
-});
 
-test("Create Instances Test Using Factory Pattern", function () {
-    expect(2);
-    var car1 = getVehicle(2000, 'Ford', 'Fusion');
-    var car2 = getVehicle(2010, 'BMW', 'Z4');
-    var expected = 'Vehicle: 2000 Ford Fusion';
-    var actual = car1.getInfo();
-    equal(actual, expected, 'Expected value: ' + expected +
-    ' Actual value: ' + actual);
-    var expected = 'Vehicle: 2010 BMW Z4';
-    var actual = car2.getInfo();
-    equal(actual, expected, 'Expected value: ' + expected +
-    ' Actual value: ' + actual);
-});
+/// <reference path="c:/Users/NMachare/Documents/html/clase 6/Scripts/_references.js" />
 
-
-test("Instance Test Using Prototype Replace Function", function () {
-    expect(2);
-    var car1 = new Vehicle(2000, 'Ford', 'Fusion');
-    var car2 = new Vehicle(2010, 'BMW', 'Z4');
-    Vehicle.prototype.getInfo = function () {
-    return 'Car: ' + this.year + ' ' + this.make + ' ' + this.model;
+module('Calculator Test Suite', {
+    setup: function () {
+        calculatorNamespace.initialize();
     }
-    var expected = 'Car: 2000 Ford Fusion';
-    var actual = car1.getInfo();
-    equal(actual, expected, 'Expected value: ' + expected +
-    ' Actual value: ' + actual);
-    var expected = 'Car: 2010 BMW Z4';
-    var actual = car2.getInfo();
-    equal(actual, expected, 'Expected value: ' + expected +
-    ' Actual value: ' + actual);
 });
+
+test("Initialize Test", function () {
+    expect(2);
+    var expected = '0';
+    equal($('#txtInput').val(), expected, 'Expected value: ' + expected +
+    ' Actual value: ' + txtInput.value);
+    equal($('#txtResult').val() , expected, 'Expected value: ' + expected +
+    ' Actual value: ' + txtResult.value);
+});
+
+
+
